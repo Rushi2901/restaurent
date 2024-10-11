a6a97cd4b5ceb0ca40dde457b196f461188297ca
rushi2901 <rwadile2003@gmail.com>
2024-10-11 16:15:09 +0530

    5th

--- restaurant/website/models.py
+++ restaurant/website/models.py
@@ -1,17 +1,42 @@
 from django.db import models
 
 # Create your models here.
-class ItemList (models.Model):
-    
+class Category (models.Model):
+    category_name =models.CharField(max_length=50,blank=False,primary_key=True)
+
+    def __str__(self):
+        return self.category_name
+
 
 class Item (models.Model):
-    pass
+    item_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    price = models.IntegerField()
+    category = models.ForeignKey(Category,related_name='item_category',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='items/')
+
+    def __str__(self):
+        return self.item_name
+
 
 class Aboutus (models.Model):
-    pass
+    description = models.TextField(blank=False)
 
 class Feedback (models.Model):
-    pass
+    user_name=models.CharField(max_length=20)
+    description= models.TextField(blank=False)
+    rating=models.IntegerField()
 
-class BookTable (models.Model):
-    pass
+    def __str__(self):
+        return self.user_name + "  rating" + self.rating
+
+
+class BookTbale (models.Model):
+    name = models.CharField(max_length=20)
+    phone_no=models.IntegerField(blank=False)
+    email=models.EmailField(blank=False)
+    total_person=models.IntegerField(blank=False)
+    booking_date=models.DateField()
+
+    def __str__(self):
+        return self.name
