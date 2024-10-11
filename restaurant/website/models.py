97816248d2e5e2c7d841638a77a0d6ca31bd14c4
rushi2901 <rwadile2003@gmail.com>
2024-10-11 15:43:56 +0530

    4th

--- restaurant/website/models.py
+++ restaurant/website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class ItemList (models.Model):
+    
+
+class Item (models.Model):
+    pass
+
+class Aboutus (models.Model):
+    pass
+
+class Feedback (models.Model):
+    pass
+
+class BookTable (models.Model):
+    pass
