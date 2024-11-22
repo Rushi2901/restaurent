1ffdd73fde466ff6257b7295e20295f5738bd1c0
rushi2901 <rwadile2003@gmail.com>
2024-11-22 06:58:08 +0530

    url chnage

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -126,15 +126,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR ,'static'),
-    ]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR ,'static'),
+    ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
