8966f1597ae49e058d84630b8a4dd12cb685eda3
rushi2901 <rwadile2003@gmail.com>
2024-11-27 17:49:17 +0530

    cloudnary test

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -146,24 +146,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Cloudinary Configuration
-cloudinary.config(
-    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.getenv('CLOUDINARY_API_KEY'),
-    api_secret=os.getenv('CLOUDINARY_API_SECRET')
-)
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+
 
 # Media storage settings using Cloudinary
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-MEDIA_URL = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-
-
-
-
-
-
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR ,'static'),
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -23,4 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls'))
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT) 
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
